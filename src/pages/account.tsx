a01312764521a3b3129d0b4295cbbbd0e63b0ed1
Colab bui <buichau40@gmail.com>
2024-05-22 19:13:58 +0700

    first commit

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Page } from 'zmp-ui';
 
 const AccountPage = () => {
+  console.log("demo");
   return (
     <div>
       <Page className="page">account page</Page>
